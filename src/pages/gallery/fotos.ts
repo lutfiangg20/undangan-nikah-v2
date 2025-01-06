35c2c31d6ae902d4c38a423ccebb5fcab6bb9fa8
lutfiangg20 <lutfiangg20@gmail.com>
2025-01-06 09:32:58 +0700

    a

--- src/pages/gallery/fotos.ts
+++ src/pages/gallery/fotos.ts
@@ -1,12 +1,21 @@
+import foto1 from "/remini2.jpg";
+import foto2 from "/remini3.jpg";
+import foto3 from "/remini4.jpg";
+import foto4 from "/remini6.jpg";
+import foto5 from "/remini1crop.jpg";
+import foto6 from "/remini7.jpg";
+import foto7 from "/remini8.jpg";
+import foto8 from "/remini9.jpg";
+import foto9 from "/remini10.jpg";
+
 export const fotos = [
-  "/remini1.jpg",
-  "/remini2.jpg",
-  "/remini3.jpg",
-  "/remini4.jpg",
-  "/remini5.jpg",
-  "/remini6.jpg",
-  "/remini7.jpg",
-  "/remini8.jpg",
-  "/remini9.jpg",
-  "/remini10.jpg",
+  foto1,
+  foto2,
+  foto3,
+  foto4,
+  foto5,
+  foto6,
+  foto7,
+  foto8,
+  foto9,
 ];
