3fbb674130e207caade8da416688654841141ed3
lutfi angga <lutfiangg20@gmail.com>
2025-01-15 07:37:49 +0700

    a

---
+++ src/components/ImageProvider.tsx
@@ -0,0 +1,41 @@
+import { createContext, useContext, useMemo } from "react";
+import foto from "/remini4.jpg";
+import kiri from "/remini8kiri.jpg";
+import kanan from "/remini8kanan.jpg";
+import foto1 from "/remini1.jpg";
+import { fotos } from "@/pages/gallery/fotos";
+import atm from "/atm.png";
+import chip from "/chip.png";
+import bca from "/bca.png";
+import bni from "/bni.svg";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const datas = {
+  foto1,
+  foto,
+  kiri,
+  kanan,
+  fotos,
+  atm,
+  chip,
+  bca,
+  bni,
+};
+
+const ImageContext = createContext<typeof datas | null>(null);
+
+export const useImages = () => {
+  return useContext(ImageContext) as typeof datas;
+};
+
+const ImageProvider = ({ children }: Props) => {
+  const images = useMemo(() => datas, []);
+  return (
+    <ImageContext.Provider value={images}>{children}</ImageContext.Provider>
+  );
+};
+
+export default ImageProvider;
